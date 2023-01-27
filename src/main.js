111433a716c0bf486f6024ddc43b05ea04762e7d
Lebarnon <53988015+Lebarnon@users.noreply.github.com>
2023-01-27 14:12:35 +0800

    Added firebase initialiser

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import '@mdi/font/css/materialdesignicons.css'
 
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './firebase/firebaseConfig'
+
 const app = createApp(App)
 
 app.use(createPinia())
@@ -18,5 +21,11 @@ app.use(createVuetify({
   components, 
   directives,
 }));
+app.use(VueFire, {
+  firebaseApp,
+  modules:[
+    VueFireAuth
+  ]
+})
 
 app.mount('#app')
