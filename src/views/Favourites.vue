462332cec537ed9f2bcc89e0caff7656d2bdacb2
Lenson <53988015+Lebarnon@users.noreply.github.com>
2023-03-06 18:20:54 +0800

    Added Favourites view

---
+++ src/views/Favourites.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container class="justify-center">
+      <v-row>
+        <v-col 
+          v-for="listing of listingStore.listings"
+          :key="listing.id"
+          cols="4"
+        >
+          <ListingCard  
+            :listing = "listing" 
+            :isFavListing= "userStore.isFavListing(listing.id)"
+            @on-fav-clicked = "handleFavClicked"
+          />
+        </v-col>
+      </v-row>   
+    </v-container>
+  </template>
+  
+  <script setup>
+  import ListingCard from "@/components/ListingCard.vue"
+  import { ref, onBeforeMount} from 'vue'
+  import { useListingStore } from "../stores/listing";
+  import { useUserStore } from "../stores/user";
+  
+  const listingStore = useListingStore()
+  const userStore = useUserStore()
+  const isLoading = ref(true)
+  
+  onBeforeMount(() => {
+    userStore.getFavListings().then(() => isLoading.value = true)
+  })
+  
+  function handleFavClicked(listingId){
+    userStore.toggleFavListing(listingId)
+  }
+    
+  </script>
+  <style scoped>
+  
+  </style>
