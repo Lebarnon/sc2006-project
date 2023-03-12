8749fdc4ff224f915d79938c2ee0cb35d86b28e0
Lebarnon <53988015+Lebarnon@users.noreply.github.com>
2023-03-12 17:55:15 +0800

    Add loading placeholders

---
+++ src/components/LoadingOverlay.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-overlay
+      :model-value="isLoading"
+      class="align-center justify-center"
+    >
+      <v-progress-circular
+        color="Black"
+        indeterminate
+        size="64"
+      ></v-progress-circular>
+    </v-overlay>
+</template>
+
+<script setup>
+const props = defineProps(["isLoading"])
+</script>
---
+++ src/components/NoResultFound.vue
@@ -0,0 +1,19 @@
+<template>
+<v-contianer class="justify-center align-center">
+  <v-row>
+    <v-img
+      :src = "SearchImg"
+    ></v-img>
+  </v-row>
+  <v-row>
+    <p class="text-h4"> Sorry! No Result Found :(</p>
+  </v-row>
+  <v-row>
+    <p class="text-subtitle">We couldn't find what you were looking for</p>
+  </v-row>
+</v-contianer>
+</template>
+
+<script setup>
+import SearchImg from '@/assets/search-illustration.png'
+</script>
