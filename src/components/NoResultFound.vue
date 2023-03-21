25af79809f33704c73ba8a7d41dcc006a7638b37
Lebarnon <53988015+Lebarnon@users.noreply.github.com>
2023-03-21 21:18:34 +0800

    Update NoResultFound.vue

--- src/components/NoResultFound.vue
+++ src/components/NoResultFound.vue
@@ -1,19 +1,32 @@
 <template>
-<v-contianer class="justify-center align-center">
-  <v-row>
+<v-contianer class="align-center">
+  <v-row class="mt-12">
     <v-img
       :src = "SearchImg"
+      width="350"
+      height="350"
     ></v-img>
   </v-row>
-  <v-row>
-    <p class="text-h4"> Sorry! No Result Found :(</p>
+  <v-row class="justify-center mt-6">
+    <p class="text-h4 "> Sorry! No Result Found :(</p>
   </v-row>
-  <v-row>
+  <v-row class="justify-center mt-6">
     <p class="text-subtitle">We couldn't find what you were looking for</p>
+  </v-row>
+  <v-row class="justify-center mt-10">
+    <v-btn
+      color="blue-darken-4"
+      @click="router.go(-1)"
+    >
+      Go Back
+    </v-btn>
   </v-row>
 </v-contianer>
 </template>
 
 <script setup>
 import SearchImg from '@/assets/search-illustration.png'
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
 </script>
